d589a455904589f0a0cdd03086d9d78c26548e78
katyarun@gmail.com <katyarun@gmail.com>
2018-05-16 01:12:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,13 +8,14 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return Object.assign(object, { [key] : value } );
 }
 
-function destructivelyDeleteFromObjectByKey(object, key) {
-  delete object[key];
-  return object;
-}
 
 function deleteFromObjectByKey(object, key) {
   var obj2 = Objects.assign( {}, object);
   delete obj2[key];
   return obj2;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object;
 }
